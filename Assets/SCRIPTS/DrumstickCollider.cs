a7b28543230a4ea80c1fa1d513449c06677a05f9
FabBautis <FabBaut@gmail.com>
2022-10-26 21:11:10 -0600

    Umm I was going to add something but I don't remember
    :do:

--- Assets/SCRIPTS/DrumstickCollider.cs
+++ Assets/SCRIPTS/DrumstickCollider.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DrumstickCollider : MonoBehaviour
 {
+    private showRotationAndPosition drumstickPosRot;
     public GameObject drumLeft;
     public GameObject drumRight;
     private AudioSource masterSound;
@@ -13,12 +15,16 @@ public class DrumstickCollider : MonoBehaviour
     private int[] patternConducting =  new int[] {0,0,0,0}; //the pattern you make
     private string firstDrumHit = null; //this just defaults a specific drum to be the main drum you hit
     private bool startedNewPattern; 
+    private bool victoryFlag;
     private int drumNumber = -1; //this is what drum you're on in the pattern
+
+    public TextMeshProUGUI patternText;
    
 
     void Start()
     {
         masterSound = GetComponent<AudioSource>();
+        patternText = patternText.GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
@@ -28,29 +34,38 @@ public class DrumstickCollider : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Drum"))
+        {
+            drumNumber++;// move to the next drum in the pattern
+            if (!startedNewPattern){
+                startedNewPattern = true;
+                firstDrumHit = other.gameObject.name;
+            }
 
-        drumNumber++;// move to the next drum in the pattern
-        if (!startedNewPattern){
-            startedNewPattern = true;
-            firstDrumHit = other.gameObject.name;
+            if (other.gameObject.name == firstDrumHit){//if the drum you hit is the main drum you need to hit
+                patternConducting[drumNumber] = 1;
+            } else {
+                patternConducting[drumNumber] = -1;
+            }
+        
+            if (other.gameObject == drumLeft){
+                masterSound.PlayOneShot(drumLeftSFX, 1.5f);
+            }
+            if (other.gameObject == drumRight){
+                masterSound.PlayOneShot(drumRightSFX, 1.5f);
+            }
+    
+            comparePatterns();
         }
 
-        if (other.gameObject.name == firstDrumHit){//if the drum you hit is the main drum you need to hit
-            patternConducting[drumNumber] = 1;
-        } else {
-            patternConducting[drumNumber] = -1;
+        if (other.gameObject.CompareTag("Acquire Info")){
+            Debug.Log("die");
+            if (drumstickPosRot.collectingInfo)
+                drumstickPosRot.collectingInfo = false;
+            else if (!drumstickPosRot.collectingInfo)
+                drumstickPosRot.collectingInfo = true;
         }
-       
-       if (other.gameObject == drumLeft){
-        Debug.Log(gameObject);
-        masterSound.PlayOneShot(drumLeftSFX, 1.5f);
-       }
-       if (other.gameObject == drumRight){
-        Debug.Log(gameObject);
-        masterSound.PlayOneShot(drumRightSFX, 1.5f);
-       }
-       
-        comparePatterns();
+        
     }
    
    void comparePatterns() //code that checks to see if you hit the drums right or not
@@ -59,12 +74,13 @@ public class DrumstickCollider : MonoBehaviour
         int actualDrum = patternConducting[drumNumber];//get the value of the drum you hit
         
         if (desiredDrum == actualDrum){
-            Debug.Log("Good Job"); //if the drum you hit is the one you're supposed to, good job. Otherwise you need to start over
+            string drumNumberString = 3-drumNumber +"";
+            patternText.text = "You need to hit " + drumNumberString + " more drums";
             if (drumNumber == 3){
                 victory();
             }
         } else if (desiredDrum != actualDrum) {
-            Debug.Log("Bad Job");
+            victoryFlag = false;
             startOverPattern();
         }
     }
@@ -76,12 +92,14 @@ public class DrumstickCollider : MonoBehaviour
         drumNumber = -1;
         startedNewPattern = false;//reset the first / main drum
         firstDrumHit = null;
-        Debug.Log("Pattern Reset");
+        if (!victoryFlag)
+            patternText.text = "Pattern Reset";
     }
 
     void victory (){
         //add more stuff here that'll happen when you do a good job IG
-        Debug.Log("YOU WIN!!!!");
+        patternText.text = "YOU WIN!!!!";
+        victoryFlag = true;
         startOverPattern();
     }
 }
--- Assets/SCRIPTS/showRotationAndPosition.cs
+++ Assets/SCRIPTS/showRotationAndPosition.cs
@@ -1,21 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class showRotationAndPosition : MonoBehaviour
 {
     public GameObject myGameObject;
-    
+    public TextMeshProUGUI textPositionRotation;
+    private double[] ghostInfoArray;
+    public bool collectingInfo;
     private Vector3 p;
     private Vector3 r;
-    
-    private float xR;
-    private float yR;
-    private float zR;
-    private float xP;
-    private float yP;
-    private float zP;
-
+    private double xR;
+    private double yR;
+    private double zR;
+    private double xP;
+    private double yP;
+    private double zP;
     private string xRString;
     private string yRString;
     private string zRString;
@@ -25,38 +27,47 @@ public class showRotationAndPosition : MonoBehaviour
 
     private string rString;
     private string pString;
-    public string info;
+    private string info;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        textPositionRotation = textPositionRotation.GetComponent<TextMeshProUGUI>();
+    }
+  
+    void collectInfo()
+    {
+       // while (collectingInfo)
+        //{
+            p = new Vector3(myGameObject.transform.position.x, myGameObject.transform.position.y, myGameObject.transform.position.z);
+            r = new Vector3(myGameObject.transform.rotation.x, myGameObject.transform.rotation.y, myGameObject.transform.rotation.z);
+            xR = Math.Round(r.x, 2);
+            yR = Math.Round(r.y, 2);
+            zR = Math.Round(r.z, 2);
+            xP = Math.Round(p.x, 2);
+            yP = Math.Round(p.y, 2);
+            zP = Math.Round(p.z, 2);
+
+            xRString = xR.ToString();
+            yRString = yR.ToString();
+            zRString = zR.ToString();
+            xPString = xP.ToString();
+            yPString = yP.ToString();
+            zPString = zP.ToString();
+            rString = "X: " + xRString + ", Y: " + yRString + ", Z: " + zRString;
+            pString = "X: " + xPString + ", Y: " + yPString + ", Z: " + zPString;
+            info = "Position: " + pString + " Rotation: " + rString;
+            textPositionRotation.text = info;
+        //}
+       
     }
 
-    void showInfo()
-    {
-        p = new Vector3(myGameObject.transform.position.x, myGameObject.transform.position.y, myGameObject.transform.position.z);
-        r = new Vector3(myGameObject.transform.rotation.x, myGameObject.transform.rotation.y, myGameObject.transform.rotation.z);
-        xR = r.x;
-        yR = r.y;
-        zR = r.z;
-        xP = p.x;
-        yP = p.y;
-        zP = p.z;
-        xRString = xR.ToString();
-        yRString = yR.ToString();
-        zRString = zR.ToString();
-        xPString = xP.ToString();
-        yPString = yP.ToString();
-        zPString = zP.ToString();
-        rString = "X: " + xRString + ", Y: " + yRString + ", Z: " + zRString;
-        pString = "X: " + xPString + ", Y: " + yPString + ", Z: " + zPString;
-        info = "Position: " + pString + " Rotation: " + rString;
-        Debug.Log(info);
-    }
+
 
 // Update is called once per frame
     void Update()
     {
-        showInfo();
+        collectInfo();
     }
 }
